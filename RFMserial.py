fcc103a97672bab46d3b6a1d0d003dfebd8e5445
jangddol <j991005@korea.ac.kr>
2024-08-09 22:20:10 +0900

    refactor: make simulation for RFMserial

--- RFMserial.py
+++ RFMserial.py
@@ -1,3 +1,4 @@
+from copy import deepcopy
 import serial
 import enum
 from channel import Channel
@@ -45,7 +46,7 @@ class CMD(enum.Enum):
     CMD_RESET = "B"
 
 
-class RFMserial:
+class RFMserial_Real:
     def __init__(self, port, baudrate):
         self.port = port
         self.baudrate = baudrate
@@ -53,99 +54,99 @@ class RFMserial:
         self.ser.reset_input_buffer()
         self.ser.reset_output_buffer()
 
-    def write(self, data):
+    def __write(self, data):
         self.ser.reset_input_buffer() # 이거 안하면 write_timeout 걸리던데 왜인지는 모름
         self.ser.write((data + '\n').encode('ascii'))
         self.ser.flush() # 다 써지기를 기다림
 
     def reset_serial(self):
-        self.write(CMD.CMD_RESET.value)
+        self.__write(CMD.CMD_RESET.value)
 
     def setReadingChannel_serial(self, channels):
         if channels[0] == Channel.CH1:
             if channels[1] == Channel.CH1:
-                self.write(CMD.CMD_SET_CH_L1M1.value)
+                self.__write(CMD.CMD_SET_CH_L1M1.value)
             elif channels[1] == Channel.CH2:
-                self.write(CMD.CMD_SET_CH_L1M2.value)
+                self.__write(CMD.CMD_SET_CH_L1M2.value)
             elif channels[1] == Channel.CH3:
-                self.write(CMD.CMD_SET_CH_L1M3.value)
+                self.__write(CMD.CMD_SET_CH_L1M3.value)
             elif channels[1] == Channel.CH4:
-                self.write(CMD.CMD_SET_CH_L1M4.value)
+                self.__write(CMD.CMD_SET_CH_L1M4.value)
             elif channels[1] == Channel.CH_UNKNOWN:
-                self.write(CMD.CMD_SET_CH_L1MU.value)
+                self.__write(CMD.CMD_SET_CH_L1MU.value)
         elif channels[0] == Channel.CH2:
             if channels[1] == Channel.CH1:
-                self.write(CMD.CMD_SET_CH_L2M1.value)
+                self.__write(CMD.CMD_SET_CH_L2M1.value)
             elif channels[1] == Channel.CH2:
-                self.write(CMD.CMD_SET_CH_L2M2.value)
+                self.__write(CMD.CMD_SET_CH_L2M2.value)
             elif channels[1] == Channel.CH3:
-                self.write(CMD.CMD_SET_CH_L2M3.value)
+                self.__write(CMD.CMD_SET_CH_L2M3.value)
             elif channels[1] == Channel.CH4:
-                self.write(CMD.CMD_SET_CH_L2M4.value)
+                self.__write(CMD.CMD_SET_CH_L2M4.value)
             elif channels[1] == Channel.CH_UNKNOWN:
-                self.write(CMD.CMD_SET_CH_L2MU.value)
+                self.__write(CMD.CMD_SET_CH_L2MU.value)
         elif channels[0] == Channel.CH3:
             if channels[1] == Channel.CH1:
-                self.write(CMD.CMD_SET_CH_L3M1.value)
+                self.__write(CMD.CMD_SET_CH_L3M1.value)
             elif channels[1] == Channel.CH2:
-                self.write(CMD.CMD_SET_CH_L3M2.value)
+                self.__write(CMD.CMD_SET_CH_L3M2.value)
             elif channels[1] == Channel.CH3:
-                self.write(CMD.CMD_SET_CH_L3M3.value)
+                self.__write(CMD.CMD_SET_CH_L3M3.value)
             elif channels[1] == Channel.CH4:
-                self.write(CMD.CMD_SET_CH_L3M4.value)
+                self.__write(CMD.CMD_SET_CH_L3M4.value)
             elif channels[1] == Channel.CH_UNKNOWN:
-                self.write(CMD.CMD_SET_CH_L3MU.value)
+                self.__write(CMD.CMD_SET_CH_L3MU.value)
         elif channels[0] == Channel.CH4:
             if channels[1] == Channel.CH1:
-                self.write(CMD.CMD_SET_CH_L4M1.value)
+                self.__write(CMD.CMD_SET_CH_L4M1.value)
             elif channels[1] == Channel.CH2:
-                self.write(CMD.CMD_SET_CH_L4M2.value)
+                self.__write(CMD.CMD_SET_CH_L4M2.value)
             elif channels[1] == Channel.CH3:
-                self.write(CMD.CMD_SET_CH_L4M3.value)
+                self.__write(CMD.CMD_SET_CH_L4M3.value)
             elif channels[1] == Channel.CH4:
-                self.write(CMD.CMD_SET_CH_L4M4.value)
+                self.__write(CMD.CMD_SET_CH_L4M4.value)
             elif channels[1] == Channel.CH_UNKNOWN:
-                self.write(CMD.CMD_SET_CH_L4MU.value)
+                self.__write(CMD.CMD_SET_CH_L4MU.value)
         elif channels[0] == Channel.CH_UNKNOWN:
             if channels[1] == Channel.CH1:
-                self.write(CMD.CMD_SET_CH_LUM1.value)
+                self.__write(CMD.CMD_SET_CH_LUM1.value)
             elif channels[1] == Channel.CH2:
-                self.write(CMD.CMD_SET_CH_LUM2.value)
+                self.__write(CMD.CMD_SET_CH_LUM2.value)
             elif channels[1] == Channel.CH3:
-                self.write(CMD.CMD_SET_CH_LUM3.value)
+                self.__write(CMD.CMD_SET_CH_LUM3.value)
             elif channels[1] == Channel.CH4:
-                self.write(CMD.CMD_SET_CH_LUM4.value)
+                self.__write(CMD.CMD_SET_CH_LUM4.value)
 
     def writeFlowSetpoint_serial(self, flowSetpoint, ch):
-        self.write(flowSetpoint)
+        self.__write(flowSetpoint)
         if ch == Channel.CH1:
-            self.write(CMD.CMD_SET_FLOW_SETPOINT_CH1.value)
+            self.__write(CMD.CMD_SET_FLOW_SETPOINT_CH1.value)
         elif ch == Channel.CH2:
-            self.write(CMD.CMD_SET_FLOW_SETPOINT_CH2.value)
+            self.__write(CMD.CMD_SET_FLOW_SETPOINT_CH2.value)
         elif ch == Channel.CH3:
-            self.write(CMD.CMD_SET_FLOW_SETPOINT_CH3.value)
+            self.__write(CMD.CMD_SET_FLOW_SETPOINT_CH3.value)
         elif ch == Channel.CH4:
-            self.write(CMD.CMD_SET_FLOW_SETPOINT_CH4.value)
+            self.__write(CMD.CMD_SET_FLOW_SETPOINT_CH4.value)
     
     def writeChannelOn_serial(self, ch):
         if ch == Channel.CH1:
-            self.write(CMD.CMD_SET_CH1_ON.value)
+            self.__write(CMD.CMD_SET_CH1_ON.value)
         elif ch == Channel.CH2:
-            self.write(CMD.CMD_SET_CH2_ON.value)
+            self.__write(CMD.CMD_SET_CH2_ON.value)
         elif ch == Channel.CH3:
-            self.write(CMD.CMD_SET_CH3_ON.value)
+            self.__write(CMD.CMD_SET_CH3_ON.value)
         elif ch == Channel.CH4:
-            self.write(CMD.CMD_SET_CH4_ON.value)
+            self.__write(CMD.CMD_SET_CH4_ON.value)
     
     def writeChannelOff_serial(self, ch):
         if ch == Channel.CH1:
-            self.write(CMD.CMD_SET_CH1_OFF.value)
+            self.__write(CMD.CMD_SET_CH1_OFF.value)
         elif ch == Channel.CH2:
-            self.write(CMD.CMD_SET_CH2_OFF.value)
+            self.__write(CMD.CMD_SET_CH2_OFF.value)
         elif ch == Channel.CH3:
-            self.write(CMD.CMD_SET_CH3_OFF.value)
+            self.__write(CMD.CMD_SET_CH3_OFF.value)
         elif ch == Channel.CH4:
-            self.write(CMD.CMD_SET_CH4_OFF.value)
+            self.__write(CMD.CMD_SET_CH4_OFF.value)
 
     def readline_serial(self):
         lf = b'\n'  # 개행 문자를 바이트로 정의
@@ -162,4 +163,94 @@ class RFMserial:
             print(f"Received non-float data")
 
         return line
+
+class RFMserial_Sim:
+    def __init__(self, port, baudrate):
+        self.channel_state = [False, False, False, False]
+        self.flow_setpoint = [0, 0, 0, 0]
+        self.channels = [Channel.CH_UNKNOWN, Channel.CH_UNKNOWN]
+
+    def reset_serial(self):
+        self.channel_state = [False, False, False, False]
+        self.flow_setpoint = [0, 0, 0, 0]
+        self.first_ch = Channel.CH_UNKNOWN
+        self.second_ch = Channel.CH_UNKNOWN
+
+    def setReadingChannel_serial(self, channels):
+        self.channels = deepcopy(channels)
+
+    def writeFlowSetpoint_serial(self, flowSetpoint, ch):
+        if ch == Channel.CH1:
+            self.flow_setpoint[0] = flowSetpoint
+        elif ch == Channel.CH2:
+            self.flow_setpoint[1] = flowSetpoint
+        elif ch == Channel.CH3:
+            self.flow_setpoint[2] = flowSetpoint
+        elif ch == Channel.CH4:
+            self.flow_setpoint[3] = flowSetpoint
+
+    def writeChannelOn_serial(self, ch):
+        if ch == Channel.CH1:
+            self.channel_state[0] = True
+        elif ch == Channel.CH2:
+            self.channel_state[1] = True
+        elif ch == Channel.CH3:
+            self.channel_state[2] = True
+        elif ch == Channel.CH4:
+            self.channel_state[3] = True
     
+    def writeChannelOff_serial(self, ch):
+        if ch == Channel.CH1:
+            self.channel_state[0] = False
+        elif ch == Channel.CH2:
+            self.channel_state[1] = False
+        elif ch == Channel.CH3:
+            self.channel_state[2] = False
+        elif ch == Channel.CH4:
+            self.channel_state[3] = False
+
+    def unparse_flow_serial_buffer(self, flow_L, flow_R):
+        _flow_L = int(float(flow_L) * 4095 / 99)
+        _flow_R = int(float(flow_R) * 4095 / 99)
+        return str(_flow_L * 100 + _flow_R / 100)
+
+    def readline_serial(self):
+        flow = [0, 0]
+        for i in range(2):
+            if self.channels[i] == Channel.CH1:
+                flow[i] = self.flow_setpoint[0]
+            elif self.channels[i] == Channel.CH2:
+                flow[i] = self.flow_setpoint[1]
+            elif self.channels[i] == Channel.CH3:
+                flow[i] = self.flow_setpoint[2]
+            elif self.channels[i] == Channel.CH4:
+                flow[i] = self.flow_setpoint[3]
+            else:
+                flow[i] = 0
+        return self.unparse_flow_serial_buffer(flow[0], flow[1])
+    
+
+class RFMserial:
+    def __init__(self, on, port, baudrate):
+        if on:
+            self.rfmserial = RFMserial_Real(port, baudrate)
+        else:
+            self.rfmserial = RFMserial_Sim(port, baudrate)
+    
+    def reset_serial(self):
+        self.rfmserial.reset_serial()
+    
+    def setReadingChannel_serial(self, channels):
+        self.rfmserial.setReadingChannel_serial(channels)
+    
+    def writeFlowSetpoint_serial(self, flowSetpoint, ch):
+        self.rfmserial.writeFlowSetpoint_serial(flowSetpoint, ch)
+    
+    def writeChannelOn_serial(self, ch):
+        self.rfmserial.writeChannelOn_serial(ch)
+    
+    def writeChannelOff_serial(self, ch):
+        self.rfmserial.writeChannelOff_serial(ch)
+    
+    def readline_serial(self):
+        return self.rfmserial.readline_serial()
